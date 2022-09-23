f46972c7b3d47db0d6b4660e21198f58b3f6f83e
mtvy <44533918+mtvy@users.noreply.github.com>
2022-09-23 10:53:37 +0300

    save changes

--- pay_bot.py
+++ pay_bot.py
@@ -14,8 +14,8 @@ from back.webhook  import proc_bot
 from back.utility  import logging
 from back.database import *
 
-from front.admin import *
-from front.user  import *
+from front.admin   import *
+from front.user    import *
 from front.utility import get_ids
 #\------------------------------------------------------------------/#
 
@@ -34,11 +34,16 @@ users_IDS : Dict[str, Any] = get_ids('users_tb')
 def start(msg : Message) -> None:
     """### Bot begin actions """
     _id = str(msg.chat.id)
-    if _id in admins_IDS.keys():
+
+    if _id in get_ids('admins_tb').keys():
         init_admin(bot, _id)
-    elif _id not in users_IDS.keys():
-        init_user(bot, _id)
+        
+    elif _id not in get_ids('users_tb').keys():
+        init_user(msg, bot, _id)
         users_IDS = get_ids('users_tb')
+    
+    #elif _id in users_IDS.keys():
+        #start_user(bot, _id)
 #\------------------------------------------------------------------/#
 
 
@@ -48,33 +53,32 @@ def start(msg : Message) -> None:
 def input_keyboard(msg : Message) -> None:
 
     @logging()
-    def _proc_call(ids : Dict[str, Any]) -> bool:
+    def __proc_call(bot : TeleBot, _id : str, ids : Dict[str, Any], txt : str) -> bool:
         if _id in ids.keys():
             USER_FUNC[txt](bot, _id)
         else:
-            txt = 'Нет доступа. Перезапустите бота /start'
             bot.send_message(_id, txt, reply_markup=rmvKey())
-
-        ...
-        ...
-        ...
-        ...
 
 
     ADMIN_FUNC = {
         'Уведомить'       : ask_accounts,
         'Добавить админа' : add_admin, 
-        'Посмотреть LTV'  : get_session_info,
-        'Админы'          : ...,
-        'Пользователи'    : ...
+        'Посмотреть LTV'  : get_session_info
     }
 
     USER_FUNC = {
-        'Мониторинг'     : ...,
+        #'Мониторинг'     : enter_monitoring,
         'Соглашение'     : ...,
         'Тех. Поддержка' : ...,
-        'Профиль'        : ...,
-        'Рефералка'      : get_refer
+        #'Профиль'        : show_prfl,
+        'Рефералка'      : ...,
+        #'Назад'          : start_user
+    }
+
+    MON_FUNC = {
+        'Аторизация' : ...,
+        #'Каналы'     : push_chnl, 
+        'Настройка'  : ..., 
     }
 
     ACC_STATUS = {
@@ -85,22 +89,19 @@ def input_keyboard(msg : Message) -> None:
     _id = str(msg.chat.id)
     txt : str = msg.text
 
-    if txt in USER_FUNC:
-        if _id in users_IDS:
-            USER_FUNC[txt](bot, _id)
-        else:
-            txt = 'Нет доступа. Перезапустите бота /start'
-            bot.send_message(_id, txt, reply_markup=rmvKey())
+    if txt in USER_FUNC.keys():
+        __proc_call(bot, _id, users_IDS, 
+            'Нет доступа. Перезапустите бота /start')
 
-    elif txt in ADMIN_FUNC:
-        if _id in admins_IDS.keys():
-            ADMIN_FUNC[txt](bot, _id)
-        else:
-            txt = 'Нет прав администратора.'
-            bot.send_message(_id, txt, reply_markup=rmvKey())
+    elif txt in ADMIN_FUNC.keys():
+        __proc_call(bot, _id, admins_IDS,
+            'Нет прав администратора.')
 
     elif txt in ACC_STATUS.keys() and _id in admins_IDS.keys():
         send_info(bot, _id, get_ids(ACC_STATUS[txt]))
+    
+    elif txt in MON_FUNC.keys() and _id in users_IDS.keys():
+        MON_FUNC[txt](bot, _id)
     
     admins_IDS, users_IDS = get_ids('admins_tb'), get_ids('users_tb')
 #\------------------------------------------------------------------/#
@@ -108,6 +109,6 @@ def input_keyboard(msg : Message) -> None:
 
 #\==================================================================/#
 if __name__ == "__main__":
-    if not proc_bot(bot):
-        bot.polling(none_stop=True)
+    #if not proc_bot(bot):
+    bot.polling(none_stop=True)
 #\==================================================================/#
